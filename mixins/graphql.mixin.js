b3489de61d145a2c049130a6708f4d476dadc5c9
shokohsc <shokohsc@gmail.com>
2021-10-17 14:48:58 +0200

    chore() development fixes

--- mixins/graphql.mixin.js
+++ mixins/graphql.mixin.js
@@ -20,7 +20,19 @@ module.exports = {
       resolvers: {}
     }
   },
-  actions: {
+  methods: {
+    startApollo($moleculer, $conn) {
+      this.controller = new ApolloServer({
+        tracing: true,
+        typeDefs: gql`${this.settings.graphql.schemas}${this.settings.graphql.queries}`,
+        resolvers: this.settings.graphql.resolvers,
+        context: async () => ({
+          $moleculer,
+          $conn
+        }),
+        plugins: [responseCachePlugin()]
+      })
+    }
   },
   async created () {
     const $conn = await r.connect({
@@ -31,27 +43,33 @@ module.exports = {
     })
     $conn.on('error', (err) => {
       this.logger.error('RethinkDB disconnected', err)
-      setTimeout(() => this.conn.reconnect(), 100)
+      setTimeout(() => this.conn.reconnect(), 1000)
     })
     this.logger.info('RethinkDB adapter has connected successfully.')
     await r.dbCreate(this.settings.rethinkdb.database).run(this.conn).catch(() => { })
     // create moleculer to pass to the context
     const $moleculer = this.broker
     // start apollo
-    this.controller = new ApolloServer({
-      tracing: true,
-      typeDefs: gql`${this.settings.graphql.schemas}${this.settings.graphql.queries}`,
-      resolvers: this.settings.graphql.resolvers,
-      context: async () => ({
-        $moleculer,
-        $conn
-      }),
-      plugins: [responseCachePlugin()]
-    })
+    this.startApollo($moleculer, $conn)
     return true
   },
   async started () {
     // TODO this.controller is sometimes undefined, why ?
+    if (!this.controller) {
+      const $conn = await r.connect({
+        host: this.settings.rethinkdb.hostname,
+        port: this.settings.rethinkdb.port,
+        db: 'ouistity',
+        silent: true
+      })
+      $conn.on('error', (err) => {
+        this.logger.error('RethinkDB disconnected', err)
+        setTimeout(() => this.conn.reconnect(), 100)
+      })
+      this.logger.info('RethinkDB adapter has connected successfully.')
+      const $moleculer = this.broker
+      this.startApollo($moleculer, $conn)
+    }
     await this.controller.listen(apollo)
     return true
   },
